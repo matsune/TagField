9e24d26d86be6a848e347cb70e2704ea5e0190f5
Yuma Matsune <yuma.matsune@gmail.com>
2018-01-25 16:58:26 +0900

    initial commit

---
+++ TagField/TagField.h
@@ -0,0 +1,19 @@
+//
+//  TagField.h
+//  TagField
+//
+//  Created by Yuma Matsune on 2018/01/25.
+//  Copyright © 2018年 matsune. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TagField.
+FOUNDATION_EXPORT double TagFieldVersionNumber;
+
+//! Project version string for TagField.
+FOUNDATION_EXPORT const unsigned char TagFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TagField/PublicHeader.h>
+
+
