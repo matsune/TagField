f1c10c61aa3d4c6567fd3866a1ad9d72eabe24ea
Yuma Matsune <yuma.matsune@gmail.com>
2018-01-25 18:58:35 +0900

    layout tagViews

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -35,7 +35,7 @@ open class PaddingLabel: UILabel {
         return CGSize(width: labelSize.width + (padding.left + padding.right),
                       height: labelSize.height + (padding.top + padding.bottom))
     }
-    
+
     open func sizeToFit(_ size: CGSize) -> CGSize {
         if intrinsicContentSize.width > size.width {
             return CGSize(width: size.width,
@@ -51,31 +51,112 @@ class TagView: PaddingLabel {
 
 final class TagField: UIScrollView {
     
+    private var tagViews: [TagView] = []
+    
+    var tagPadding: UIEdgeInsets = .zero {
+        didSet {
+            tagViews.forEach { $0.padding = tagPadding }
+        }
+    }
+    
+    var tagBackgroundColor: UIColor = .clear {
+        didSet {
+            tagViews.forEach { $0.backgroundColor = tagBackgroundColor }
+        }
+    }
+    
+    var padding: UIEdgeInsets = .zero {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+    
+    var tagBetweenSpace: CGFloat = 2.0
+    
+    var lineBetweenSpace: CGFloat = 3.0
+    
+    var intrinsicContentHeight: CGFloat = 50
+
+    func addTag(text: String) {
+        let tagView = createTagView(text: text)
+        addSubview(tagView)
+        tagViews.append(tagView)
+        setNeedsLayout()
+    }
+    
+    private func createTagView(text: String) -> TagView {
+        let tagView = TagView()
+        tagView.text = text
+        return tagView
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        repositionSubviews()
+    }
+    
+    private func repositionSubviews() {
+        let fullWidth = bounds.width - (padding.left + padding.right)
+        
+        var numOfLines = 1
+        var maxHeightOfLine: CGFloat = 0
+        
+        var x: CGFloat = padding.left
+        var y: CGFloat = padding.top
+        for tagView in tagViews {
+            let tagSize = tagView.intrinsicContentSize
+            let spaceWidth = fullWidth - x
+            
+            if tagSize.width > spaceWidth {
+                // new line
+                x = padding.left
+                y += maxHeightOfLine + lineBetweenSpace
+                maxHeightOfLine = 0
+                numOfLines += 1
+                
+                if tagSize.width > fullWidth {
+                    // cripping
+                    tagView.frame = CGRect(x: x, y: y, width: fullWidth, height: tagSize.height)
+                } else {
+                    tagView.sizeToFit()
+                    tagView.frame.origin = CGPoint(x: x, y: y)
+                }
+            } else {
+                tagView.sizeToFit()
+                tagView.frame.origin = CGPoint(x: x, y: y)
+            }
+            x += tagSize.width + tagBetweenSpace
+            maxHeightOfLine = max(maxHeightOfLine, tagSize.height)
+        }
+    }
 }
 
 final class ViewController: UIViewController {
 
+    var d = TagField(frame: CGRect(x: 0, y: 100, width: 100, height: 100))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-//        let v = TagView(frame: CGRect(x: 100, y: 50, width: 100, height: 10))
-//        v.text = "aaaaaa"
-//        v.backgroundColor = .orange
-//        view.addSubview(v)
+        view.backgroundColor = .gray
         
-        let d = TagView(frame: CGRect(x: 100, y: 230, width: 100, height: 10))
-        d.text = "aaaaaa"
-        d.backgroundColor = .orange
-        d.padding = UIEdgeInsets(top: 10, left: 10, bottom: 20, right: 0)
-        d.sizeToFit()
+        d.backgroundColor = .white
+        d.padding = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        d.addTag(text: "aaa")
+        d.addTag(text: "aa")
+        d.addTag(text: "aaaaaaaaaaaaaaaaa")
+        d.addTag(text: "aaadasfdfsads")
+        d.addTag(text: "aaaddd")
+        d.tagBackgroundColor = .orange
+        
         view.addSubview(d)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+//
+//        }
     }
-
 
 }
 
