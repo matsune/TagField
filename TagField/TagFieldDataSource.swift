cd878efc761f626ed6a3725ea9c3d819229fa235
Yuma Matsune <yuma.matsune@gmail.com>
2018-04-23 17:25:23 +0900

    change optional

--- TagField/TagFieldDataSource.swift
+++ TagField/TagFieldDataSource.swift
@@ -10,21 +10,14 @@ import Foundation
 
 public protocol TagFieldDataSource: class {
     func tagField(_ tagField: TagField, classForTagAt index: Int) -> TagView.Type
-    func tagField(_ tagField: TagField, styleForTagAt index: Int) -> TagStyle
+    func tagField(_ tagField: TagField, styleForTagAt index: Int) -> TagStyle?
     func tagField(_ tagField: TagField, interTagSpacingAt index: Int) -> CGFloat
     func tagField(_ tagField: TagField, sideInsetAtLine line: Int) -> (left: CGFloat, right: CGFloat)
 }
 
 public extension TagFieldDataSource {
-    func tagField(_ tagField: TagField, styleForTagAt index: Int) -> TagStyle {
-        return TagStyle {
-            $0.padding = UIEdgeInsets(top: 4, left: 2, bottom: 3, right: 4)
-            $0.normalTextColor = .white
-            $0.normalBackgroundColor = .orange
-            $0.selectedTextColor = .white
-            $0.selectedBackgroundColor = .red
-            $0.cornerRadius = 8.0
-        }
+    func tagField(_ tagField: TagField, styleForTagAt index: Int) -> TagStyle? {
+        return nil
     }
     
     func tagField(_ tagField: TagField, classForTagAt index: Int) -> TagView.Type {
